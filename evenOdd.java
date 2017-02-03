afcde04eefa617fd8bbf768d2c1433fe3a20224a
rizzryan <ryanwelch.nsa@gmail.com>
2017-02-03 14:51:53 -0500

    Solution for evenOdd

--- evenOdd.java
+++ evenOdd.java
@@ -2,9 +2,9 @@ public static void main(String[] args)
 {
   public int[] evenOdd(int[] nums)
   {
-    int n = 0;
+    int n = 0; //The index variable
     while(n < nums.length && nums[n] % 2 == 0)
-        n++;
+        n++; //Index gets incremented
     for(int j = n + 1; j < nums.length; j++)
     {
         if(nums[j] % 2 == 0)
@@ -12,9 +12,9 @@ public static void main(String[] args)
           int temp = nums[n];
           nums[n] = nums[j];
           nums[j] = temp;
-          n++;
-        }
-    }
-    return nums;
+          n++; //n gets incremented
+        } //End if statement
+    } //End for loop
+    return nums; //Array gets returned
   }
 }
